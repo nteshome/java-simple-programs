7c748636cd792d851b48075589b4a48e177c0eee
nteshome <97781440+nteshome@users.noreply.github.com>
2022-11-16 19:59:45 +0300

    Add files via upload
    using java wap input to numbers and print  sum

---
+++ nati.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+class nati
+ {
+    public static void main(String[] agrs){
+   Scanner input =new Scanner(System.in);
+   int x;
+int y;
+int sum;
+ x=input.nextInt();
+   y=input.nextInt();
+   sum=input.nextInt();
+   System.out.print("enter two number");
+   x=input.nextInt();
+   y=input.nextInt();
+   sum=x+y;
+   System.out.printf("sum is %d %n ",sum);
+}
+    }
